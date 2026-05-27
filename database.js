74553cded0405c2c0df0fac18844f54d83bad50b
cbsouza20 <2025363@student.cct.ie>
2026-05-27 21:53:28 +0100

    Updating design and adding code comments

--- database.js
+++ database.js
@@ -1,19 +1,29 @@
+// Import MySQL2 package
 const mysql = require("mysql2");
 
+// Create database connection configuration
 const db = mysql.createConnection({
+    // Database host name
     host: "localhost",
+    // MySQL username
     user: "root",
+    // MySQL password
     password: "M1l@152090",
+    // Database name used in the project
     database: "fitlife_store"
 });
 
+// Connect application to MySQL database
 db.connect((err) => {
+
+    // Check if connection failed
     if (err) {
         console.log("Database connection failed:", err);
         return;
     }
-
+    // Display successful connection message
     console.log("Connected to MySQL database");
 });
 
+// Export database connection to other files
 module.exports = db;
